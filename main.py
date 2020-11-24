d425bee9650cee54f17571d23706f809831b37dd
PedroRuizCode <pedro.ruiz@javeriana.edu.co>
2020-11-24 09:59:04 -0500

    Add files

---
+++ main.py
@@ -0,0 +1,13 @@
+'''         Image processing and computer vision
+                  Pedro Elí Ruiz Zárate
+               Electronic engineering student
+              Pontificia Universidad Javeriana
+                      Bogotá - 2020
+'''
+from Bandera import *
+
+if __name__ == '__main__':
+    a = Bandera()
+    a.Colores()
+    a.Porcentaje()
+    a.Orientacion()
